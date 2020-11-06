2d34ba849b459418dede9d0bcd1301e86fcb601d
Deshagette josue <71509166+JosueDesha@users.noreply.github.com>
2020-11-06 18:02:57 +0000

    etape 4

---
+++ src/main/java/ManagerBean.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@Named("manager")
+@RequestScoped
+public class ManagedBean {
+
+    private String version = "1.0-SNAPSHOT";
+   
+
+    public String getVersion() {
+        return version;
+    }
+    public int id;
+    public String lastName;
+    public String firstName;
+    public String birthDate;
+    public String phoneNumber;
+
+    public ArrayList<Student> getStudentList() 
+    {
+        ArrayList<Student> studentList = new ArrayList<Student>(
+                Arrays.asList(new Student(1, "desha", "josue", "12/09/2003", "0696123456"),
+                        new Student(2, "JHONSON", "Derick", "12/09/2000", "0696123456")));
+
+        return studentList;
+    }
+}
---
+++ src/main/java/student.java
@@ -0,0 +1,62 @@
+public class Student 
+    {
+        
+        public int id;
+        public String lastName;
+        public String firstName;
+        public String birthDate;
+        public String phoneNumber;
+    
+        public int getId() {
+            return this.id;
+        }
+    
+        public void setId(int id) {
+            this.id = id;
+        }
+    
+        public String getLastName() {
+            return this.lastName;
+        }
+    
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    
+        public String getFirstName() {
+            return this.firstName;
+        }
+    
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+    
+        public String getBirthDate() {
+            return this.birthDate;
+        }
+    
+        public void setBirthDate(String birthDate) {
+            this.birthDate = birthDate;
+        }
+    
+        public String getPhoneNumber() {
+            return this.phoneNumber;
+        }
+    
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    
+        
+        public Student(int id, String lastName, String firstName, String birthDate, String phoneNumber) {
+    
+          this.id =id;
+            this.lastName=lastName;
+            this.firstName=firstName;
+            this.birthDate=birthDate;
+            this.phoneNumber=phoneNumber;
+    
+            
+        }
+    
+}
